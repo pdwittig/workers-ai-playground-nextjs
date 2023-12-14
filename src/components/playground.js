cb40fb775b8336dc3bd95619584d3406f5367806
Phil Wittig <pwittig@cloudflare.com>
2023-12-13 22:00:17 -0800

    remove non functional features

--- src/components/playground.js
+++ src/components/playground.js
@@ -78,8 +78,8 @@ export default function PlaygroundPage() {
       </div>
       <div className="hidden h-full flex-col md:flex rounded-[0.5rem] border bg-background shadow">
         <div className="container flex flex-col items-start justify-between space-y-2 py-4 sm:flex-row sm:items-center sm:space-y-0 md:h-16">
-          <h2 className="text-lg font-semibold">Playground</h2>
-          <div className="ml-auto flex w-full space-x-2 sm:justify-end">
+          <h2 className="text-lg font-semibold">Workers AI Playground</h2>
+   {/*       <div className="ml-auto flex w-full space-x-2 sm:justify-end">
             <PresetSelector presets={presets} />
             <PresetSave />
             <div className="hidden space-x-2 md:flex">
@@ -87,7 +87,7 @@ export default function PlaygroundPage() {
               <PresetShare />
             </div>
             <PresetActions />
-          </div>
+          </div>*/}
         </div>
         <Separator />
         <Tabs defaultValue="complete" className="flex-1">
