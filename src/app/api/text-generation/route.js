7bbb616cb7013c740e547eaa1d852a68f958f706
Phil Wittig <pwittig@cloudflare.com>
2023-12-21 16:11:51 -0800

    add baselime

--- src/app/api/text-generation/route.js
+++ src/app/api/text-generation/route.js
@@ -1,21 +1,40 @@
 import { StreamingTextResponse } from 'ai';
 import { Ai } from '@cloudflare/ai';
 import { WorkersAiStream } from '../../lib/WorkersAiStream';
+import { BaselimeLogger } from "@baselime/edge-logger"
+
 
 export const runtime = 'edge';
 // This is required to enable streaming
 export const dynamic = 'force-dynamic';
 
-export async function POST(request) {
+export async function POST(request, context) {
   const ai = new Ai(process.env.AI);
   const { prompt, model, maxTokens } = await request.json();
 
+  // log({ promp, model, maxTokens }, context);
+  
   const res = await ai.run(model, {
     prompt,
     max_tokens: maxTokens,
     stream: true,
   });
 
+
   const stream = WorkersAiStream(res);
   return new StreamingTextResponse(stream);
+}
+
+
+const log = (data, context) => {
+  const logger = new BaselimeLogger({
+    service: "playground",
+    namespace: context.request.url,
+    apiKey: context.env.BASELIME_API_KEY,
+    ctx: context,
+    isLocalDev: context.env.IS_LOCAL,
+  });
+
+  logger.info("LLM inference", { data });
+  context.waitUntil(logger.flush());
 }
